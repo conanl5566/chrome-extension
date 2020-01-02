0f320a379316c99632658f22506d1e0e98163d6d
conanl <conanl@CONANL-PC>
2020-01-02 16:55:23 +0800

    站点下指定页面不可用

--- background.js
+++ background.js
@@ -1,6 +1,41 @@
 function test() {
     alert("test");
+
+  
+    //chrome.webNavigation.onDOMContentLoaded.addListener(details => {
+    //    chrome.pageAction.show(details.tabId);
+
+    //})
+
+
+    //chrome.tabs.getSelected(null, function (tab) {
+
+    //    chrome.pageAction.show(tab.id);
+
+
+    //});
+
 }
+
+
+//chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//    if (changeInfo.status == 'complete') {
+
+//        alert("LOADED");
+//        //setTimeout(runExtension, 1000);
+
+//        chrome.webNavigation.onDOMContentLoaded.addListener(details => {
+//            chrome.pageAction.show(details.tabId);
+
+//        })
+
+
+//    }
+//})
+
+
+
+
 
 //chrome.extension.onMessage.addListener(function (objRequest, _, sendResponse) {
 //    var strText = objRequest.txt;
@@ -52,3 +87,64 @@ chrome.runtime.onMessage.addListener(function (request, sender, callback) {
     //}, 2000);
     return true;//这是重点，没有return true，上面返回的是undefined而不是"hi!"
 });
+
+
+
+chrome.runtime.onInstalled.addListener(function () {
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+
+     //   alert(1);
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                // That fires when a page's URL contains a 'g' ...
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { urlContains: 's?' }, //url的内容中包含字母g的，插件才会被激活
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }
+        ]);
+    });
+});
+
+
+
+chrome.tabs.onUpdated.addListener(function (id, info, tab) {
+    if (tab.status === 'loading') {
+        //updateBrowserAction(id, tab.url);
+
+     
+        if (tab.url.indexOf('s?') != -1) {
+            //alert(tab.url);
+            //   alert(444);
+           
+        }
+        else {
+            alert(tab.id);
+            chrome.pageAction.hide(tab.id);
+        }
+
+
+    }
+});
+
+
+
+//chrome.tabs.onActivated.addListener(function (activeInfo) {
+//    if (activeInfo.tabId) {
+//        chrome.tabs.get(activeInfo.tabId, function (tab) {
+//            //updateBrowserAction(tab.id, tab.url);
+//            alert(333);
+//            if (tab.url.index('s?') != -1) {
+
+//                alert(444);
+//                chrome.pageAction.hide(tab.id);
+//            }
+
+//        });
+//    }
+//});
--- content.js
+++ content.js
@@ -8,4 +8,18 @@ $(function () {
 
         });
     });
+
+    $("#button2").click(function () {
+        // 先获取background页面
+        var bg = chrome.extension.getBackgroundPage();
+        //再在返回的对象上调用background.js 里面的函数
+        bg.test();
+    });
+
+
+    //// 先获取background页面
+    //var bg = chrome.extension.getBackgroundPage();
+    ////再在返回的对象上调用background.js 里面的函数
+    //bg.test();
+
 })
